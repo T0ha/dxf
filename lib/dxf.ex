ed171c1d4c9a654318a1f520a379e81c38e2f4b8
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2025-04-28 19:56:31 +0500

    mix project

---
+++ lib/dxf.ex
@@ -0,0 +1,18 @@
+defmodule Dxf do
+  @moduledoc """
+  Documentation for `Dxf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dxf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
