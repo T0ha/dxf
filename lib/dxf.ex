5c48d4ddfa9d2459bc9ff67cd90fdbda59528879
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2025-05-13 12:37:45 +0500

    Header section parser works

--- lib/dxf.ex
+++ lib/dxf.ex
@@ -2,17 +2,69 @@ defmodule Dxf do
   @moduledoc """
   Documentation for `Dxf`.
   """
+  alias Dxf.Section.Header
+
+  @type t() :: %__MODULE__{
+          header: list(),
+          tables: map(),
+          blocks: map(),
+          entities: map(),
+          objects: map()
+        }
+
+  defstruct [
+    :tables,
+    :blocks,
+    :entities,
+    :objects,
+    header: %{}
+  ]
+
+  @entity_type "0"
+  @name "2"
+
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dxf.hello()
-      :world
-
+  Parses a DXF file and returns a map of sections.
   """
-  def hello do
-    :world
+  def parse_file(file_path) do
+    file_path
+    |> File.read!()
+    |> parse()
+  end
+
+  
+  def parse(data) do
+    data
+    |> String.split("\r\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> parse_tagged()
+  end
+
+  def parse_tagged(acc), do: parse_tagged(acc, %__MODULE__{})
+
+  def parse_tagged([], acc), do: acc
+
+  def parse_tagged([@entity_type, type, @name, name | rest], acc) do
+    parse_type(type, name, rest, acc)
+  end
+
+  #defp parse_tagged([@name, name | rest], acc, nil) do
+  #  parse_entity(type, name, rest, acc)
+  #end
+
+  def parse_tagged([tag, data | rest], acc) do
+    {{tag, data}, rest, acc}
+  end
+
+  defp parse_type(_, type, rest, acc) do
+    case type do
+      "HEADER" -> Header.parse(rest, acc)
+      #"TABLES" -> parse_tables(rest, acc)
+      #"BLOCKS" -> parse_blocks(rest, acc)
+      #"ENTITIES" -> parse_entities(rest, acc)
+      #"OBJECTS" -> parse_objects(rest, acc)
+      _ -> {:error, "Unknown section: #{type}"}
+    end
   end
 end
---
+++ lib/section/header.ex
@@ -0,0 +1,28 @@
+defmodule Dxf.Section.Header do
+  @moduledoc """
+  Header section of a DXF file.
+  """
+
+  @type t() :: %{}
+
+
+  @entity_type "0"
+  @variable "9"
+
+  @spec parse([String.t()], map()) :: map()
+  def parse([@entity_type, "ENDSEC" | rest], acc) do
+    Dxf.parse_tagged(rest, acc)
+  end
+
+  def parse([@variable, name | rest], acc) do
+    {value, rest} = parse_value(rest, [])
+    new_acc = put_in(acc.header[name], value)
+    parse(rest, new_acc)
+  end
+
+  @spec parse_value([String.t()], [String.t()]) :: {map(), [String.t()]}
+  def parse_value([@entity_type, "ENDSEC" | _rest] = rest, acc), do: {acc, rest}
+  def parse_value([@variable | _rest] = rest, acc), do: {acc, rest}
+  def parse_value([type, value | rest], acc), do:
+    parse_value(rest, [{type, value} | acc])
+end
