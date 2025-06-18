1266b09336f0150092601b221e79b842c90b6be9
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2025-06-18 17:03:20 +0200

    Added simple tests

--- test/dxf_test.exs
+++ test/dxf_test.exs
@@ -3,6 +3,8 @@ defmodule DxfTest do
   doctest Dxf
 
   test "greets the world" do
-    assert %Dxf{} = Dxf.parse_file("../../data/project_part.dxf")
+    assert %Dxf{} =
+             Dxf.parse_file("../../data/project_part.dxf")
+             |> IO.inspect(limit: :infinity, pretty: true)
   end
 end
